25014db82fe759130a4ef48bd6143e9e6dd37e3c
nguyen-hoang-phong <phongdc24v7x106@dttx.ctu.edu.vn>
2025-10-16 08:13:17 +0700

    Resolve merge conflict in .gitignore

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
+const contacts = require("../controllers/contact.controller");
+
 const router = express.Router();
 
-// Ví dụ route
-router.get('/', (req, res) => {
-  res.send('Contact route is working!');
-});
+router
+  .route("/")
+  .get(contacts.findAll)
+  .post(contacts.create)
+  .delete(contacts.deleteAll);
+
+router.route("/favorite").get(contacts.findAllFavorite);
+
+router
+  .route("/:id")
+  .get(contacts.findOne)
+  .put(contacts.update)
+  .delete(contacts.delete);
 
 module.exports = router;
